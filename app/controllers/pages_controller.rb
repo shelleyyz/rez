56c2c0f27373468b2d9d781a9a6fe78acb4f1989
Shelley Zhang <shelleyyz91@gmail.com>
2018-08-29 23:45:46 +1000

    regex added but not working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,13 +22,28 @@ class PagesController < ApplicationController
 
   def chat
     @request = params[:chatContent] #assigning input to request
+
+    if @request.length < 1
+          @request = "(Nothing was asked.)"
+    end
+
     result = $dialogflow.text_request @request #posting request and getting response back from dialogflow
     @response = result[:result][:fulfillment][:speech] #getting correct data from JSON and assigning to response
-
     @current_user.conversations.create :request => @request, :response => @response #saving request and response to the conversations table / database . Then creating a conversation and saving it to the current user with user id.
 
     redirect_back :fallback_location => root_path #rails require fallback location otherwise it will error
 
     #have to remove turbolinks so that it doesn't refresh the page everytime a post request starts
   end
+
+  def convert_url
+    regex_url = /https?:\/\/[\S]+/
+    match = regex_url.match(@response)
+    if @response.match(regex_url)
+      @converted_response = @response.sub!(regex_url, '<a href="{$url[0]}">{$url[0]}</a>', @response)
+    else
+      @converted_response = @response
+    end
+#convert the plain text url received from response and convert it into html hyperlink
+    end
 end
