8c8603835d32634429a0f2836934aa8a7cf10bb1
Shelley Zhang <shelleyyz91@gmail.com>
2018-09-03 15:34:08 +1000

    minor styling changes to chat home and login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,17 +9,6 @@ class PagesController < ApplicationController
     # raise
   end
 
-#get question from params from form submission - form has route that is controlled by method in controller
-
-#get answer/response from dialogflow
-#append answer to page
-
-# button / form => route which hears the button click
-#  the route should point to a controller method
-#  the controller method shoulf get the Q from params and send it to $dialoglfow
-#  the answer is returned from dialogflow
-# Q and A should be appeneded to the page someoheow
-
   def chat
     @request = params[:chatContent] #assigning input to request
 
